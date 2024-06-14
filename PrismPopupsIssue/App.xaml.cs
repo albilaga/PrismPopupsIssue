d2762beaf8b7d3288c548ff784a751a6a017ada2
albilaga <albilaga@pm.me>
2024-06-14 13:17:48 +0700

    Initial commit

---
+++ PrismPopupsIssue/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace PrismPopupsIssue;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ PrismPopupsIssue/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace PrismPopupsIssue;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
